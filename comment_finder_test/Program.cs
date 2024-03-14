fdbbaeca1a6149fc75794bb942037e0ae3f87090
hunterdyar <bloop@hdyar.com>
2024-03-14 18:22:46 -0400

    testing

--- comment_finder_test/Program.cs
+++ comment_finder_test/Program.cs
@@ -1,22 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using comment_finder_test;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-Console.WriteLine("Hello, Program!");
-
 var code = new StreamReader("../../../code_to_analyze/test.cs");
 
 SyntaxTree tree = CSharpSyntaxTree.ParseText(code.ReadToEnd());
-CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+//var comments = root.DescendantTrivia().OfType<SyntaxTrivia>().Where(st=>!st.IsKind(SyntaxKind.WhitespaceTrivia) && !st.IsKind(SyntaxKind.EndOfLineTrivia)).ToList();
+//var nodes = root.DescendantNodes(x => true, true).ToList();
 
-var comment = root.DescendantTrivia().OfType<SyntaxTrivia>().Where(st=>!st.IsKind(SyntaxKind.WhitespaceTrivia) && !st.IsKind(SyntaxKind.EndOfLineTrivia));
-var nodes = root.DescendantNodes(x => true, true).ToList();
-
-var comments = new List<string>();
-foreach (var triv in comment)
+var walker = new Walker();
+var newTree = walker.Visit(tree.GetRoot());
+var newCode = newTree.ToFullString();
+foreach (var c in walker.Comments)
 {
-	comments.Add(triv.ToFullString());
+Console.Write(c.Comment);
 }
+Console.WriteLine("-----");
+Console.Write(newCode);
 
+
+//the main challenge is that using CodeAnalysis gives us a tree walker to use, but separating our 'docs' from the code will require more linear, less recursive approach.
+//luckily, extending TreeWalker gives us a walk in the correct order. So we need to walk the tree, and for every node that is a comment, we can add it to a docs list, with a reference to it's associated token.
+//then, we render the code and render the comments.... and somehow find that relevant token to figure out what group or node we belong to? I think?
+
+
+//Hmmm, looks like maybe copying the comments out using TextSpan into docs might be the way to go.
+//or the way that gobyexample does it, which is to not use the SyntaxTree and, instead, to use regex.
+//which would, honestly, be fine. Any edge-cases we can test and re-write.
---
+++ comment_finder_test/Walker.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace comment_finder_test;
+
+public class Walker : CSharpSyntaxRewriter
+{
+	public List<Doc> Comments = new List<Doc>();
+
+	public struct Doc
+	{
+		public string Comment;
+		public Location Location;
+	}
+	public override SyntaxTrivia VisitTrivia(SyntaxTrivia trivia)
+	{
+		if (trivia.IsKind(SyntaxKind.EndOfLineTrivia) || trivia.IsKind(SyntaxKind.WhitespaceTrivia))
+		{
+			return trivia;
+		}
+		else
+		{
+			Comments.Add(new Doc()
+			{
+				Comment = trivia.ToString(),
+				Location = trivia.GetLocation(),
+			});
+			//store a comment and some respective node id.
+		}
+		//replace trivia with empty.
+		return new SyntaxTrivia();
+		return base.VisitTrivia(trivia);
+	}
+}
