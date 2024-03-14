129e0e987c74dc9b6f165d2b33653651c7713fa7
hunterdyar <bloop@hdyar.com>
2024-03-14 17:15:40 -0400

    Initial commit

---
+++ comment_finder_test/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+Console.WriteLine("Hello, Program!");
+
+var code = new StreamReader("../../../code_to_analyze/test.cs");
+
+SyntaxTree tree = CSharpSyntaxTree.ParseText(code.ReadToEnd());
+CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+var comment = root.DescendantTrivia().OfType<SyntaxTrivia>().Where(st=>!st.IsKind(SyntaxKind.WhitespaceTrivia) && !st.IsKind(SyntaxKind.EndOfLineTrivia));
+var nodes = root.DescendantNodes(x => true, true).ToList();
+
+var comments = new List<string>();
+foreach (var triv in comment)
+{
+	comments.Add(triv.ToFullString());
+}
+
