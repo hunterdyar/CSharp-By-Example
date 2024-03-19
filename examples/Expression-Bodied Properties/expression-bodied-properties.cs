113b8004a0ce187944b57c4c0b6adea5df1e1a93
hunterdyar <bloop@hdyar.com>
2024-03-19 10:43:17 -0400

    expression-bodied clarity

--- examples/Expression-Bodied Properties/expression-bodied-properties.cs
+++ examples/Expression-Bodied Properties/expression-bodied-properties.cs
@@ -1,29 +1,21 @@
-//Expression-Bodied properties let you define an implementation in a consise way. 
-//They serve as an alternative syntax for getters, and reduct the number of one-line functions you may write.
+
 using Systems.Collections.Generic;
 using System.Linq;
 public class ExpressionBodiedExample
 {
-	
+	//Expression-Bodied properties let you define an implementation in a consise way.
+	//They serve as an alternative syntax for getters and reduce the need to write one-line functions.
 	public string member => expression;
-	
-	//
-	
-	//_name is private get/set, and Names is public get-only. This is a convenient pattern. 
-	//In Unity, now one could apply attributes too the private member, like [SerializeField].
+	//---
 	private List<string> _names;
-	public List<string> Names => _names;
+	//They are often used for convenience and simplicity.
 	public int Count => _names.Count;
+	public bool AnyNames => _names != null && _names.Count > 0;
+	//In Unity, one could apply attributes to a private member, like [SerializeField], and use an expression-bodied property for a public getter.
+	public List<string> Names => _names;
 	
-	//Expreession-bodied members work well with LINQ expressions, which can often accomplish a lot of functionality in a single expression.  
-	//See also: LINQ
+	//Expreession-bodied members work well with LINQ expressions, which can often accomplish a lot of functionality in a single line.
 	public int Franks => _names.Where(x => x == "Frank").Count();
 	public List<string> GetNamesCapitalized() => _names.Select(n => Char.ToUpperInvariant(n[0]) + n.Substring(1).ToLower()).ToList();
-
-	//this is another way to accomplish a public-getter private-setter approach. I find it more difficult to intuit about.
-	public List<string> OldNames; {get private set; }
 	
-	//
-//  
-
 }
