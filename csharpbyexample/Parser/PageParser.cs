19fa1252fb8277c7c3e3ad3e5b876c0d0fdb3020
hunterdyar <bloop@hdyar.com>
2024-03-22 10:55:34 -0400

    links object created;

--- csharpbyexample/Parser/PageParser.cs
+++ csharpbyexample/Parser/PageParser.cs
@@ -121,8 +121,7 @@ public class PageParser
 		using (StreamReader sr = new StreamReader(yamlFileInfo.FullName))
 		{
 			var yaml = await sr.ReadToEndAsync();
-			var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
-				.WithNamingConvention(PascalCaseNamingConvention.Instance).Build();
+			var deserializer = new DeserializerBuilder().Build();
 			_page.Meta = deserializer.Deserialize<PageMeta>(yaml);
 			
 		}
--- csharpbyexample/SiteDescription/PageMeta.cs
+++ csharpbyexample/SiteDescription/PageMeta.cs
@@ -1,9 +1,12 @@
-﻿namespace CSharpByExample;
+﻿using YamlDotNet.Serialization;
+
+namespace CSharpByExample;
 
 public class PageMeta
 {
 	public int Order = 9001;//default is later order
 	public bool Draft = false;
 	public bool HasLinks => Links.Length > 0;//this is just a convenience for the mustache template.
-	public string[] Links = Array.Empty<string>();
+	
+	public Link[] Links = Array.Empty<Link>();
 }
