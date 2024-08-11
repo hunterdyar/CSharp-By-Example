fd5d479a1147766f505ebbaffc93f0ea74edf45e
hunterdyar <bloop@hdyar.com>
2024-08-11 19:59:24 -0400

    overflow hidden

--- examples/Functions/functions.cs
+++ examples/Functions/functions.cs
@@ -1,15 +1,15 @@
 public class FunctionExample {
-    
+
     //Functions are named blocks of re-usable code.
     //If a function belongs to a class, like these do, we call it a 'Method'.
-    
+
     //Functions are defined much like variables: a return type then a name. After the name, we use parentheses.
     //The parentheses are how C# know's it's a function. Inside them, we put parameters, variables used by the function.
     public string Emphasize(string parameter)
     {
         return parameter+"!";
     }
-    //---
+
     //If we don't define a scope, they are private by default.
     //If we don't return a value, we have to use the "void" placeholder as the functions return type.
     void Banana()
@@ -18,11 +18,11 @@ public class FunctionExample {
         string ouput = Emphasize("Bananas");
         Console.WriteLine(ouput);
     }
-    
+
     //Functions can have any number of arguments.
     public int SumFourThings(int a, int b,int c, int d)
     {
-        return a+b;
+        return a+b+c+d;
     }
 
     public void Test()
